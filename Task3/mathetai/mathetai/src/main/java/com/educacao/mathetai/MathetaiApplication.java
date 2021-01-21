3b3a783f391f10af4c3c6ec59ce7953e34c65818
vnalmenara <veronica_n_almenara@hotmail.com>
2021-01-21 11:49:01 -0300

    Task3-Criacao-Spring

---
+++ Task3/mathetai/mathetai/src/main/java/com/educacao/mathetai/MathetaiApplication.java
@@ -0,0 +1,13 @@
+package com.educacao.mathetai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathetaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathetaiApplication.class, args);
+	}
+
+}
---
+++ Task3/mathetai/mathetai/src/test/java/com/educacao/mathetai/MathetaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educacao.mathetai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathetaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
