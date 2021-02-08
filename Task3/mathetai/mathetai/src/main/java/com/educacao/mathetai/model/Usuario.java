a73fe0204da2baf6460b1177fa13a043157aaed0
LaisLimaSantos <laissp25@gmail.com>
2021-02-08 12:14:32 -0300

    deploy, swagger e relacionamento usuario

--- Task3/mathetai/mathetai/src/main/java/com/educacao/mathetai/model/Usuario.java
+++ Task3/mathetai/mathetai/src/main/java/com/educacao/mathetai/model/Usuario.java
@@ -1,12 +1,18 @@
 package com.educacao.mathetai.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_usuario")
@@ -29,11 +35,10 @@ public class Usuario {
 
 	private String contato;
 
-	/*
-	 * @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
-	 *
-	 * @JsonIgnoreProperties("usuario") private List<Postagem> postagem;
-	 */
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
+
 	public long getId() {
 		return id;
 	}
@@ -74,11 +79,12 @@ public class Usuario {
 		this.contato = contato;
 	}
 
-	/*
-	 * public List<Postagem> getPostagem() { return postagem; }
-	 *
-	 * public void setPostagem(List<Postagem> postagem) { this.postagem = postagem;
-	 * }
-	 */
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
 
 }
