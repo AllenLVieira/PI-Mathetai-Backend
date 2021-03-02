c9e25bdeef00db460ce5b77d618d800dfc49e1fc
vnalmenara <veronica_n_almenara@hotmail.com>
2021-03-02 20:03:58 -0300

    criacao-arquivo-sql-com-temas

--- Task3/mathetai/mathetai/src/main/java/com/educacao/mathetai/model/Tema.java
+++ Task3/mathetai/mathetai/src/main/java/com/educacao/mathetai/model/Tema.java
@@ -3,12 +3,14 @@ package com.educacao.mathetai.model;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -24,6 +26,7 @@ public class Tema {
 
 	@NotNull
 	@Size(min = 5, max = 50)
+	@Column(unique=true)
 	private String assunto;
 
 	@OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
